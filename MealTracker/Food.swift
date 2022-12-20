f19838e9894561bd00bed73ec2967afe49af63b6
Christian Manzaraz <magnusmanz@gmail.com>
2022-12-20 12:56:49 -0300

    Create Model Objects

---
+++ MealTracker/Food.swift
@@ -0,0 +1,12 @@
+//
+//  Food.swift
+//  MealTracker
+//
+//  Created by Christian Manzaraz on 20/12/2022.
+//
+
+import Foundation
+
+struct Food {
+    var name: String, description: String
+}
---
+++ MealTracker/Meal.swift
@@ -0,0 +1,12 @@
+//
+//  Meal.swift
+//  MealTracker
+//
+//  Created by Christian Manzaraz on 20/12/2022.
+//
+
+import Foundation
+
+struct Meal {
+    var name: String, food: [Food]
+}
